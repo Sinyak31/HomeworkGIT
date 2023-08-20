5b2478390b821a318b8e0649db32b71d20804b8e
Sinyak31 <Sindyai31@gmail.com>
2023-08-20 11:15:06 +0400

    changing the sout in the experiment branch

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Переключился на ветку master");
+        System.out.println("Новая ветка experiment");
+    }
+}
