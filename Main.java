593f5c2261f0d3e03b4f7b685d2b669cc5d83197
Sinyak31 <Sindyai31@gmail.com>
2023-08-20 10:52:39 +0400

    adding sout

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Переключился на ветку master");
-
+        System.out.println("отправка на удаленный репозиторий");
     }
 }
